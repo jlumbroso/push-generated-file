60779e7e93e96fa4279180da8adb821950624658
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2021-04-23 02:05:07 -0400

    More refactoring of the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,9 +6,9 @@
 
 
 # Informational prompt
-echo "**************************"
+echo "=========================="
 echo "PUSH TO REPO GITHUB ACTION"
-echo "**************************"
+echo "=========================="
 echo ""
 echo "Git version: $(git --version)"
 echo "SSH version: $(ssh --version)"
@@ -100,27 +100,27 @@ if [ "${VAR_GIT_MODE}" = "ssh" ]
 then
   echo "<> Configuring SSH settings"
 
-  echo "  * Creating SSH configuration folder"
+  echo "  - Creating SSH configuration folder"
   mkdir -p ~/.ssh
 
-  echo "  * Storing provided SSH private key"
+  echo "  - Storing provided SSH private key"
   echo "$INPUT_AUTH_SSH_KEY" > ~/.ssh/id_key
   chmod u=rw,go= ~/.ssh/id_key
 
-  echo "  * Attempting to generate public SSH key pair"
+  echo "  - Attempting to generate public SSH key pair"
   ssh-keygen -y -f ~/.ssh/id_key > ~/.ssh/id_key.pub
 
-  echo "  * Public key to be used: $(cat ~/.ssh/id_key.pub)"
+  echo "  - Public key to be used: $(cat ~/.ssh/id_key.pub)"
 
   # display visual fingerprint
   ssh-keygen -lvf ~/.ssh/id_key.pub
   
-  echo "  * Configuring Git SSH command"
+  echo "  - Configuring Git SSH command"
   export GIT_SSH_COMMAND="ssh -i ~/.ssh/id_key -o IdentitiesOnly=yes -o UserKnownHostsFile=~/.ssh/known_hosts -o BatchMode=yes"
   echo "   GIT_SSH_COMMAND='$GIT_SSH_COMMAND'"
   
   # known hosts
-  echo "  * Creating local known hosts file"
+  echo "  - Creating local known hosts file"
   touch ~/.ssh/known_hosts
   ssh-keyscan github.com >> ~/.ssh/known_hosts
   ssh-keyscan gitlab.com >> ~/.ssh/known_hosts
@@ -128,7 +128,7 @@ then
   cat ~/.ssh/known_hosts
   
   # ssh config to avoid push timeout
-  echo "  * Creating SSH config file"
+  echo "  - Creating SSH config file"
   # See https://bengsfort.github.io/articles/fixing-git-push-pull-timeout/
   # See https://docs.gitlab.com/ee/topics/git/troubleshooting_git.html#check-your-ssh-configuration
   cat > ~/.ssh/config <<EOL
@@ -142,7 +142,7 @@ Host github.com
 EOL
  
   # set local file permissions
-  echo "  * Set local file permissions"
+  echo "  - Set local file permissions"
   chmod 700 ~/.ssh
   chmod 644 ~/.ssh/config
   chmod 644 ~/.ssh/known_hosts
@@ -160,12 +160,12 @@ echo "<> Cloning destination git repository"
 if [ "$VAR_GIT_MODE" = "local" ]
 then
   # git auth: local mode
-  echo "  * Using local authentication (no authentication)"
+  echo "  - Using local authentication (no authentication)"
 
   { # try
     git clone --single-branch --branch "$INPUT_TARGET_BRANCH" "${VAR_CLONE_DIR}"
   } || { # on no such remote branch, pull default branch instead
-    echo "  * The input target branch does not already exist on the target repository. It will be created."
+    echo "  - The input target branch does not already exist on the target repository. It will be created."
     git clone --single-branch "${VAR_CLONE_DIR}"
     VAR_TARGET_BRANCH_EXISTS=false
   }
@@ -173,13 +173,13 @@ then
 elif [ "$VAR_GIT_MODE" = "https" ]
 then
   # git auth: https mode
-  echo "  * Using HTTPS authentication with GitHub personal access token"
+  echo "  - Using HTTPS authentication with GitHub personal access token"
 
   # username/password
   { # try
     git clone --single-branch --branch "$INPUT_TARGET_BRANCH" "https://$INPUT_AUTH_GITHUB_TOKEN@github.com/$INPUT_DESTINATION_REPO.git" "${VAR_CLONE_DIR}"
   } || { # on no such remote branch, pull default branch instead
-    echo "  * The input target branch does not already exist on the target repository. It will be created."
+    echo "  - The input target branch does not already exist on the target repository. It will be created."
     git clone --single-branch "https://$INPUT_AUTH_GITHUB_TOKEN@github.com/$INPUT_DESTINATION_REPO.git" "${VAR_CLONE_DIR}"
     VAR_TARGET_BRANCH_EXISTS=false
   }
@@ -187,81 +187,107 @@ then
 elif [ "$VAR_GIT_MODE" = "ssh" ]
 then
   # git auth: ssh mode
-  echo "  * Using SSH private deploy key"
+  echo "  - Using SSH private deploy key"
 
   { # try
     git clone --single-branch --branch "$INPUT_TARGET_BRANCH" "git@github.com:$INPUT_DESTINATION_REPO.git" "${VAR_CLONE_DIR}"
   } || { # on no such remote branch, pull default branch instead
-    echo "  * The input target branch does not already exist on the target repository. It will be created."
+    echo "  - The input target branch does not already exist on the target repository. It will be created."
     git clone --single-branch "git@github.com:$INPUT_DESTINATION_REPO.git" "${VAR_CLONE_DIR}"
     VAR_TARGET_BRANCH_EXISTS=false
   }
 
 fi
 
-echo "  * Contents of cloned directory at ${VAR_CLONE_DIR}"
+echo "  - Contents of cloned directory tree at ${VAR_CLONE_DIR}"
 ls -la "${VAR_CLONE_DIR}"
 
 
-echo "Copying files to git repo. Invisible files must be handled differently than visible files."
-# Include dot files for source filepath
+echo "<> Moving files to destination folder [invisible files must be handled differently than visible files]"
+
+echo "  - Ensuring existence of destination folder in cloned tree"
 mkdir -p ${VAR_CLONE_DIR}/$INPUT_DESTINATION_FOLDER
 
-if [ $(find "$INPUT_SOURCE_FILE_PATH" -type f | wc -l) -gt 0 ]; then
+# Include dot files for source filepath
+if [ $(find "$INPUT_SOURCE_FILE_PATH" -not -ipath '*.git/*' -type f | wc -l) -gt 0 ]; then
+  echo "  - Detected the presence of visible files"
   ##cp -r "$INPUT_SOURCE_FILE_PATH"/* "${VAR_CLONE_DIR}/$INPUT_DESTINATION_FOLDER"
   mv "$INPUT_SOURCE_FILE_PATH"/* "${VAR_CLONE_DIR}/$INPUT_DESTINATION_FOLDER"
 else
-  echo "WARNING: No visible files exist"
-fi
-invisible_exists=false
-if test -f "$INPUT_SOURCE_FILE_PATH"/.??*; then
-  ##cp -r "$INPUT_SOURCE_FILE_PATH"/.??* "${VAR_CLONE_DIR}/$INPUT_DESTINATION_FOLDER"
-  mv "$INPUT_SOURCE_FILE_PATH"/.??* "${VAR_CLONE_DIR}/$INPUT_DESTINATION_FOLDER"
-else
-  echo "WARNING: No invisible/hidden (dot) files exist"
+  echo "  - WARNING: No visible files detected"
 fi
 
+VAR_INVISIBLE_EXISTS=false
+if test -f "$INPUT_SOURCE_FILE_PATH"/.??*; then
+  echo '  - Detected the presence of invisible files (.*)'
+  ##cp -r "$INPUT_SOURCE_FILE_PATH"/.??* "${VAR_CLONE_DIR}/$INPUT_DESTINATION_FOLDER"
+  mv "$INPUT_SOURCE_FILE_PATH"/.??* "${VAR_CLONE_DIR}/$INPUT_DESTINATION_FOLDER"
+  VAR_INVISIBLE_EXISTS=true
+else
+  echo '  - No invisible files (.*) detected'
+fi
+
+echo "  - Contents of cloned directory tree at ${VAR_CLONE_DIR}"
 cd "${VAR_CLONE_DIR}"
 ls -la
 
+
 # Create branch locally if it doesn't already exist locally
 if [ "${VAR_TARGET_BRANCH_EXISTS}" = false ] ; then
+  echo "<> Creating branch '${INPUT_TARGET_BRANCH}' locally"
   git checkout -b "$INPUT_TARGET_BRANCH"
 fi
 
-echo "Adding git commit"
+echo "<> Preparing commits"
 
 COMMITS_BY_PUSH=2
 FILES_BY_COMMIT=50
+
+echo "   [commits_by_push: ${COMMITS_BY_PUSH}, files_by_commit: ${FILES_BY_COMMIT}]"
+
+# variables to keep track of the number of files added and commits mde
 ADDED=0
 COMMIT_ID=0
+
 find . -not -ipath '*.git/*' -type f | while read file
 do
+  # if the git add is unsuccessful, skip to next loop
   git add "$file" || continue
+
+  # if the git add is successful, we have one more file added
   ADDED=$((ADDED+1))
+
+  # is it time to make a commit?
   if [ $((ADDED % FILES_BY_COMMIT)) -eq 0 ]
   then
-    echo "Attempting a commit (added: ${ADDED}, files_by_commit:${FILES_BY_COMMIT})"
+    echo "  - Attempting a commit (added: ${ADDED}, files_by_commit:${FILES_BY_COMMIT})"
     
-    git diff-index --quiet HEAD || git commit --message "Update (${COMMIT_ID}) from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA" && COMMIT_ID=$((COMMIT_ID+1))
+    # git diff-index to avoid an error when there are no changes to commit
+    git diff-index --quiet HEAD || {
+      git commit --message "Update (${COMMIT_ID}) from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+      COMMIT_ID=$((COMMIT_ID+1))
+    }
   fi
+
+  # is it time to make a push?
   if [ $((COMMIT_ID % COMMITS_BY_PUSH)) -eq 0 ]
   then
-    echo "Attempting a push (commits: ${COMMIT_ID}, commits_by_push:${COMMITS_BY_PUSH})"
+    echo "  - Attempting a push (commits: ${COMMIT_ID}, commits_by_push:${COMMITS_BY_PUSH})"
+    # --set-upstream also creates the branch if it doesn't already exist in the destination repository
     git push origin --set-upstream "$INPUT_TARGET_BRANCH"
   fi
 done
-#commit remaining
+
+# commit remaining
+
+echo "<> Commit and push any remaining files"
 git add .
 git status
-# git diff-index to avoid an error when there are no changes to commit
 git diff-index --quiet HEAD || git commit --message "Update remaining files from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-
-##git add .
-##git status
-# git diff-index to avoid an error when there are no changes to commit
-##git diff-index --quiet HEAD || git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-
-echo "Pushing git commit. Create branch if none exists."
-# --set-upstream also creates the branch if it doesn't already exist in the destination repository
 git push origin --set-upstream "$INPUT_TARGET_BRANCH"
+
+
+echo "DONE!"
+echo ""
+
+echo "Don't forget to star the repo for this action! :-)"
